560772e79a60ee853c3fd32fdd182c3ceb2d2067
32Ivan <ivan.govic32@gmail.com>
2020-10-21 23:52:51 +0200

    Updates

---
+++ precache-manifest.0b4e894f6ddd3f6e3caa70794bc0fdf7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ba24510cf25842fc0403c1ddc7e055",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "1f3b41b90bd881544fca",
+    "url": "/test/static/css/main.7609077a.chunk.css"
+  },
+  {
+    "revision": "2111f5ed0191e7f54b80",
+    "url": "/test/static/js/2.682570c8.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/test/static/js/2.682570c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3b41b90bd881544fca",
+    "url": "/test/static/js/main.7af8fecc.chunk.js"
+  },
+  {
+    "revision": "eefaebae6292c11585c4",
+    "url": "/test/static/js/runtime-main.0c1c99e6.js"
+  },
+  {
+    "revision": "c105b2bab33abdcc19b9c13394d234b2",
+    "url": "/test/static/media/MotionSensor.c105b2ba.jpg"
+  },
+  {
+    "revision": "97382493c12028127424a801cc868485",
+    "url": "/test/static/media/SmartHub.97382493.jpg"
+  },
+  {
+    "revision": "c814870d2e00dca5cb992f919e307cc4",
+    "url": "/test/static/media/SmokeSensor.c814870d.jpg"
+  },
+  {
+    "revision": "fa306bb8a5ee50640b12dfa7fdc4a55b",
+    "url": "/test/static/media/WaterLeakSensor.fa306bb8.jpg"
+  },
+  {
+    "revision": "0211f849670398c94bed81f3f5923615",
+    "url": "/test/static/media/WirelessCamera.0211f849.jpg"
+  },
+  {
+    "revision": "64ba16f3c05f7b13af3358fe2bf3bc19",
+    "url": "/test/static/media/cover.64ba16f3.jpg"
+  }
+]);
